8a0bb2d2d7d5d06c698fa2b5b74f57df7899cde3
Arnav Dashaputra <arugold1@gmail.com>
2022-03-20 12:56:22 -0400

    fix: cleanup

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/*
-	The7 by TEMPLATE STOCK
-	templatestock.co @templatestock
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
 $(document).ready(function () {
 
 	/**
